4d83bc3f2834749a28bbe1028ca8bc66057397e8
K Aditya <165629293+Aditya-K-369@users.noreply.github.com>
2025-06-07 22:28:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
-
+const port = 3000 || process.env.PORT;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -55,6 +55,6 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-server.listen(3000, '0.0.0.0', () => {
+server.listen(port, '0.0.0.0', () => {
   console.log("✅ Server running on http://localhost:3000");
 });
